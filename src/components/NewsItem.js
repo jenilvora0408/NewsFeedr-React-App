359f53d3b7520da7b9e8127435c8e8f2b04ff244
Jenil Vora <85816884+jenilvora0408@users.noreply.github.com>
2022-03-21 10:21:52 +0530

    Using fetch API to populate NewsItems

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,10 +6,18 @@ export class NewsItem extends Component {
     return (
       <div className="my-3">
         <div className="card" style={{ width: "18rem" }}>
-          <img src={imageUrl} className="card-img-top" alt="..." />
+          <img
+            src={
+              !imageUrl
+                ? "https://s.yimg.com/ny/api/res/1.2/Zq3bC5VXltc_vLzyuWV.ZQ--/YXBwaWQ9aGlnaGxhbmRlcjt3PTEyMDA7aD04MDA-/https://s.yimg.com/uu/api/res/1.2/q__LwMQ9RGQa5oRqC4Ok9Q--~B/aD0yNjY3O3c9NDAwMDthcHBpZD15dGFjaHlvbg--/https://media.zenfs.com/en/aol_yahoo_finance_433/1a212983159fbf5ffdac7fb98b3bbb7f"
+                : imageUrl
+            }
+            className="card-img-top"
+            alt="..."
+          />
           <div className="card-body">
-            <h5 className="card-title">{title}... </h5>
-            <p className="card-text">{description}... </p>
+            <h5 className="card-title">{title} </h5>
+            <p className="card-text">{description} </p>
             <a
               href={newsUrl}
               target="_blank"
