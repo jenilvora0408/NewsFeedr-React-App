66bd369095c3ee059f2557f2fa2c1154d771428f
Jenil Vora <85816884+jenilvora0408@users.noreply.github.com>
2022-04-09 09:52:15 +0530

    Changing class-based components to functional-based components

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,53 +1,50 @@
-import React, { Component } from "react";
+import React from "react";
 
-export class NewsItem extends Component {
-  render() {
-    let { title, description, imageUrl, newsUrl, author, date, source } =
-      this.props;
-    return (
-      <div className="my-3">
-        <div className="card">
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "flex-end",
-              position: "absolute",
-              right: "0",
-            }}
+const NewsItem = (props) => {
+  let { title, description, imageUrl, newsUrl, author, date, source } = props;
+  return (
+    <div className="my-3">
+      <div className="card">
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-end",
+            position: "absolute",
+            right: "0",
+          }}
+        >
+          <span className="badge rounded-pill bg-dark">{source}</span>
+        </div>
+
+        <img
+          src={
+            !imageUrl
+              ? "https://s.yimg.com/ny/api/res/1.2/Zq3bC5VXltc_vLzyuWV.ZQ--/YXBwaWQ9aGlnaGxhbmRlcjt3PTEyMDA7aD04MDA-/https://s.yimg.com/uu/api/res/1.2/q__LwMQ9RGQa5oRqC4Ok9Q--~B/aD0yNjY3O3c9NDAwMDthcHBpZD15dGFjaHlvbg--/https://media.zenfs.com/en/aol_yahoo_finance_433/1a212983159fbf5ffdac7fb98b3bbb7f"
+              : imageUrl
+          }
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{description} </p>
+          <p className="card-text">
+            <small className="text-muted">
+              By {!author ? "unknown" : author} on{" "}
+              {new Date(date).toGMTString()}{" "}
+            </small>
+          </p>
+          <a
+            href={newsUrl}
+            // target="_blank"
+            className="btn btn-sm btn-primary"
           >
-            <span className="badge rounded-pill bg-dark">{source}</span>
-          </div>
-
-          <img
-            src={
-              !imageUrl
-                ? "https://s.yimg.com/ny/api/res/1.2/Zq3bC5VXltc_vLzyuWV.ZQ--/YXBwaWQ9aGlnaGxhbmRlcjt3PTEyMDA7aD04MDA-/https://s.yimg.com/uu/api/res/1.2/q__LwMQ9RGQa5oRqC4Ok9Q--~B/aD0yNjY3O3c9NDAwMDthcHBpZD15dGFjaHlvbg--/https://media.zenfs.com/en/aol_yahoo_finance_433/1a212983159fbf5ffdac7fb98b3bbb7f"
-                : imageUrl
-            }
-            className="card-img-top"
-            alt="..."
-          />
-          <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{description} </p>
-            <p className="card-text">
-              <small className="text-muted">
-                By {!author ? "unknown" : author} on{" "}
-                {new Date(date).toGMTString()}{" "}
-              </small>
-            </p>
-            <a
-              href={newsUrl}
-              // target="_blank"
-              className="btn btn-sm btn-primary"
-            >
-              Read More
-            </a>
-          </div>
+            Read More
+          </a>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default NewsItem;
